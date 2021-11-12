b4e880b7ea3d8171e13bfab1a2f61006ea7bed18
andrija-s <51809572+andrija-s@users.noreply.github.com>
2021-11-11 19:51:43 -0800

    more lint

--- source/genericFetch.js
+++ source/genericFetch.js
@@ -1,14 +1,14 @@
 export class genericFetchClass {
-    constructor(input) {
-        this.options = input;
-        this.data = {};
-    }
+  constructor(input) {
+    this.options = input
+    this.data = {}
+  }
 
-    static async genericFetchFunc(input) {
-        await axios.request(input.options).then(function (response) {
-            input.data = response.data;
-        }).catch(function (error) {
-            console.log(error);
-        })
-    }
+  static async genericFetchFunc(input) {
+    await axios.request(input.options).then(function (response) {
+      input.data = response.data
+    }).catch(function (error) {
+      console.log(error)
+    })
+  }
 }
